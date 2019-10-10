d55e9bf5ed16809d2168c2f2021027468310a911
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2019-10-10 17:05:56 +0200

    closes #3. Progress on #5 #7

--- R/make_jagscode.R
+++ R/make_jagscode.R
@@ -1,248 +1,69 @@
 #' Make JAGS code for Multiple Change Point model
 #'
-#' @param data A data.frame or tibble containing the variables expressed in `segments` in long format.
-#' @param segments A vector of formulas - one for each segment. The left-hand side specifies the form of the change point (on x). The right-hand side specifices the form of intercepts and slopes. See `mcp` examples for details.
-#' @param prior A named list with parameter names as names and a JAGS distribution as value, e.g., `list(int_1 = "dunif(10, 30)")`.
-#' @param param_x A string. Only relevant if no segments contains slope (no hint at what x is). Set this, e.g., param_x = "time".
+#' @param data data.frame or tibble containing the variables expressed in `segments` in long format.
+#' @param prior Named list with parameter names as names and a JAGS distribution as value, e.g., `list(int_1 = "dunif(10, 30)")`.
+#' @param formula_jags String. The formula for computing param_y given param_x and all predictors. Returned by `unpack_segments()`
+#' @param param_x string
+#' @param param_y String
 #' @keywords jags, model
-#' @export
-#' @examples
 #'
-make_jagscode = function(data, segments, prior, param_x = NULL) {
-
-  # General handy values
-  nsegments = length(segments)
-  if(nsegments == 0) stop("At least one segment is needed")
-
-  all_pars = list()
-  for(i in 1:nsegments) all_pars[paste0("segment_", i)] = list()
-
-  # Figure out the name of the y-axis an the x-axis
-  param_name_y = as.character(segments[[1]][[2]])
-  if(param_name_y == "1") {
-    stop("response cannot be '1'. Must be a column in data.")
-  }
-
-  all_slopes = c()
-  for(i in 1:nsegments) {
-    slope_name = attr(terms(segments[[i]]), "term.labels")
-    if(!is.null(slope_name)) all_slopes = c(all_slopes, slope_name)
-  }
-  if(!length(all_slopes)) {
-    if(!is.null(param_x)) param_name_x = param_x
-    else stop("This is a plateau-only model, but `param_x` is missing: mcp(..., param_x = \"name\")")
-  } else param_name_x = all_slopes[1]
-
-  # Check that only one slope has been used
-  if(length(unique(all_slopes)) > 1) stop(paste0("More than one slope predictor in the segments: ", paste0(all_slopes, collapse = ", ")))
-
-
+make_jagscode = function(data, prior, formula_jags, nsegments, sample, param_x, param_y) {
   # Begin model. `mm` is short for "mcp model".
-  # Make it global so that mm_add can manipulate it.
-  mm <<- ""
-
   # Add fixed variables.
-  mm_add("
+
+
+  mm = paste0("
   data {
     # X values
-    MINX = ", min(data[, param_name_x]), "
-    MAXX = ", max(data[, param_name_x]), "
-    MEANX = ", mean(unlist(data[, param_name_x])), "
-    SDX = ", sd(unlist(data[, param_name_x])), "
+    MINX = ", ifelse(sample, min(data[, param_x]), "[requires data]"), "
+    MAXX = ", ifelse(sample, max(data[, param_x]), "[requires data]"), "
+    MEANX = ", ifelse(sample, mean(unlist(data[, param_x])), "[requires data]"), "
+    SDX = ", ifelse(sample, sd(unlist(data[, param_x])), "[requires data]"), "
 
     # Y values
-    MINY = ", min(data[, param_name_y]), "
-    MAXY = ", max(data[, param_name_y]), "
-    MEANY = ", mean(unlist(data[, param_name_y])), "
-    SDY = ", sd(unlist(data[, param_name_y])), "
+    MINY = ", ifelse(sample, min(data[, param_y]), "[requires data]"), "
+    MAXY = ", ifelse(sample, max(data[, param_y]), "[requires data]"), "
+    MEANY = ", ifelse(sample, mean(unlist(data[, param_y])), "[requires data]"), "
+    SDY = ", ifelse(sample, sd(unlist(data[, param_y])), "[requires data]"), "
   }
 
-  model {"
-  )
+  model {
+    # Priors\n")
+
+  # Add all priors
+  prior_vector = unlist(prior)
+  prior_str = paste0(names(prior_vector), " ~ ", prior_vector, collapse="\n    ")
+  mm = paste0(mm, prior_str)
+  mm = paste0(mm, "
+    cp_0 = -10^100  # mcp helper value; minus infinity
+    cp_", nsegments, " = 10^100  # mcp helper value; plus infinity\n
+    ")
 
 
-  # Add prior on sigma
-  if("sigma" %in% names(prior)) {
-    par_value = prior[["sigma"]]
-  } else {
-    par_value = "dnorm(0, 1 / SDY^2) T(0, )  # mcp default"
-  }
-  mm_add("\n
-    # Prior on sigma (standard deviation of likelihood function)
-    sigma ~ ", par_value)
-  all_pars["sigma"] = "sigma"
-
-
-  # Add priors on change points
-  mm_add("\n\n  # Priors on change points. Enforces order.\n")
-  mm_add("  cp_0 = -10^100  # mcp helper value; minus infinity\n")
-  if(nsegments == 1) cp_loop = c()  # No change point for 1-segment models
-  else cp_loop = 1:(nsegments - 1)
-  for(i in cp_loop) {
-    param_name = paste0("cp_", i)
-    all_pars[param_name] = param_name
-
-    # If prior is manually specified
-    if(param_name %in% names(prior)) {
-      if(i > 1) trunc = paste0(" T(cp_", i-1, ", )")
-      else trunc = ""
-      par_value = paste0(prior[[param_name]], trunc)
-    }
-
-    # Otherwise use default priors
-    else {
-      if(i == 1) par_value = "dunif(MINX, MAXX)"
-      else if(i == nsegments) par_value = paste0("dunif(cp_", i-1, ", MAXX)")
-      else par_value = paste0("dunif(cp_", i-1, ", MAXX)")
-      par_value = paste0(par_value, "  # mcp default")  # Comment
-    }
-
-    # Add new line to mm
-    new_prior = paste0("  ", param_name, " ~ ", par_value, "\n")
-    mm_add(new_prior)
-  }
-  mm_add("  cp_", i+1, " = 10^100  # mcp helper value; plus infinity\n")
-
-
-  # Population-priors on intercepts
-  mm_add("\n  # Priors on intercepts\n")
-
-  for(i in 1:nsegments) {
-    # If there is an intercept
-    if(attr(terms(segments[[i]]), "intercept")) {
-      param_name = paste0("int_", i)
-      all_pars[[paste0("segment_", i)]]["int_free"] = param_name
-
-      # If the prior is manually specified
-      if(param_name %in% names(prior)) {
-        par_value = prior[[param_name]]
-      }
-
-      # Otherwise use default priors
-      else {
-        par_value = "dt(0, 1 / (SDY * 3)^2, 1)  # mcp default"
-      }
-
-      # Add it to the model
-      new_prior = paste0("  ", param_name, " ~ ", par_value, "\n")
-      mm_add(new_prior)
-    }
-  }
-
-
-  # Population priors on slopes
-  mm_add("\n  # Priors on slopes\n")
-
-  for(i in 1:nsegments) {
-    # If there is a slope
-    if(length(attr(terms(segments[[i]]), "term.labels"))) {
-
-      # Set parameter name
-      param_name = paste0(param_name_x, "_", i)
-      all_pars[[paste0("segment_", i)]]["slope_free"] = param_name
-
-      # If there is an explicit prior...
-      if(param_name %in% names(prior)) {
-        par_value = prior[[param_name]]
-      }
-
-      # Otherwise use default priors
-      else {
-        par_value = "dt(0, 1 / (SDY / (MAXX - MINX) * 3)^2, 1)  # mcp default"
-      }
-
-      # Add it to the model
-      new_prior = paste0("  ", param_name, " ~ ", par_value, "\n")
-      mm_add(new_prior)
-    }
-  }
-
-
-
-  # TO DO
   # * Build list of parameters involved in each segment
-  mm_add("
-    # Model and likelihood
-    for(i_ in 1:length(", param_name_x, ")) {
+  mm = paste0(mm, "\n
+  # Model and likelihood
+  for(i_ in 1:length(", param_x, ")) {
 
-      # Additive segments
-      y_[i_] = \n")
+    # Fitted value
+    y_[i_] = \n")
 
-  for(i in 1:nsegments) {
-    segment = all_pars[[paste0("segment_", i)]]
-    segment_pars = names(segment)
-
-    if(!is.null(segment_pars)) {  # If there are parameters for this segment
-      # Indicator whether this segment is included
-      mm_add("             (", param_name_x, "[i_] > cp_", i - 1, ") * (")
-
-      # Add intercept of this segment
-      has_int = "int_free" %in% segment_pars
-      has_slope = "slope_free" %in% segment_pars
-      if(has_int) {
-        mm_add("int_", i)
-      }
-
-      # Add int and slope if both are in this segment
-      if(has_int & has_slope) mm_add(" + ")
-
-      # Add slope of this segment
-      if(has_slope) {
-        mm_add(param_name_x, "_", i, " * (min(", param_name_x, "[i_], cp_", i, ") ")
-        if(i > 1) mm_add("- cp_", i - 1)
-        mm_add(")")
-      }
-
-      # Close line
-      if(i < nsegments) mm_add(") +  #segment ", i, "\n")  # More terms to come
-      else mm_add(")  # segment ", i)  # This was the last
-    }
-    else {
-      if(i < nsegments) mm_add("             0 +  #segment ", i, "\n")  # More terms to come
-      else mm_add("             0  # segment ", i)  # This was the last
-    }
-  }
+  # Add JAGS code from `unpack_segments` and indent it
+  mm = paste0(mm, gsub("\n", "\n      ", formula_jags))
 
   # Finally the likelihood
-  mm_add("\n\n    # Likelihood
-      ", param_name_y, "[i_] ~ dnorm(y_[i_], 1 / sigma^2)")
+  mm = paste0(mm, "\n\n      # Likelihood
+      ", param_y, "[i_] ~ dnorm(y_[i_], 1 / sigma^2)")
 
   # Log-density.
-  mm_add("\n\n    # Log-density for WAIC computation
-      loglik_[i_] = logdensity.norm(", param_name_y, "[i_], y_[i_], 1 / sigma^2)")
+  mm = paste0(mm, "\n\n      # Log-density for LOO/WAIC computation
+      loglik_[i_] = logdensity.norm(", param_y, "[i_], y_[i_], 1 / sigma^2)")
 
   # Finish up
-  mm_add("
+  mm = paste0(mm, "
     }
   }")
 
-  # RETURN IT ALL
-  # Convert all_pars to a simple vector of parameter names
-  all_pars = unlist(all_pars)
-  names(all_pars) = NULL
-
-
-  return(list(
-    model = mm,
-    all_pars = all_pars,
-    param_name_x = param_name_x,
-    param_name_y = param_name_y
-  ))
-}
-
-
-
-
-#' Quick paste
-#'
-#' @param ... Terms to be paste0'ed to the string `mm`.
-#' @export
-#' @examples
-#' mm = "model{\n"
-#' mm_add('  sigma = dnorm(', mu, ', ' 1 /', sigma, '^2)\n')
-#' mm_add('\}')
-#' cat(mm)
-#'
-mm_add = function(...) {
-  mm <<- paste0(mm, ...)
+  # Return the model
+  mm
 }
---
+++ R/unpack_segments.R
@@ -0,0 +1,165 @@
+#' Understand the list of segments
+#'
+#' @param segments A vector of formulas - one for each segment. The left-hand side specifies the form of the change point (on x). The right-hand side specifices the form of intercepts and slopes. See `mcp` examples for details.
+#' @param prior A named list with parameter names as names and a JAGS distribution as value, e.g., `list(int_1 = "dunif(10, 30)")`.
+#' @param param_x A string. Only relevant if no segments contains slope (no hint at what x is). Set this, e.g., param_x = "time".
+#' @keywords jags, model
+#'
+
+
+unpack_segments = function(segments, prior = list(), param_x = NULL) {
+  ##############
+  # PREPROCESS #
+  ##############
+  # Detect param_x
+  if(is.null(param_x)) {
+    param_xs = lapply(segments, function(x) attr(terms(x), "term.labels"))
+    param_xs = param_xs[!param_xs %in% c("rel(1)", "rel(0)")]  # intercepts are not param_x
+    # Reduce to only one: remove replicated. c(x, rel(x), rel(1), rel(0)) becomes "x"
+    param_x = unique(gsub("rel\\(|\\)", "", unlist(param_xs)))
+  }
+
+  # Detect param_y
+  param_y = as.character(segments[[1]][[2]])
+
+
+
+  ##########
+  # CHECKS #
+  ##########
+  if(length(segments) == 0) {
+    stop("At least one segment is needed")
+  }
+
+  if(is.null(param_x)) {
+    stop("This is a plateau-only model, but `param_x` is missing in `mcp`")
+  }
+  if(length(param_x) > 1) {
+    stop(paste0("More than one slope variable in segment formulas: ", paste0(param_x, collapse=",")))
+  }
+
+  if(param_y %in% c("0", "1")) {
+    stop("Response cannot be 0 or 1. Must be a column in data.")
+  }
+
+  default_prior_slope = "dt(0, 1 / (SDY / (MAXX - MINX) * 3)^2, 1)"
+  default_prior_int = "dt(0, 1 / (SDY * 3)^2, 1)"
+
+
+  # Build list of priors and a formula
+  default_prior = list(sigma = "dnorm(0, 1 / SDY^2) T(0, )")
+  formula_str = ""
+
+  for(i in 1:length(segments)) {
+    # RHS as list of chars. Used to detect intercepts and slopes
+    RHS = stringr::str_trim(unlist(strsplit(as.character(segments[[i]])[[3]], "\\+")))
+    LHS = stringr::str_trim(unlist(strsplit(as.character(segments[[i]])[[2]], "\\+")))
+
+
+    # Settings
+    has_intercept = any(c("1", "rel(1)") %in% RHS)
+    rel_param_x = paste0("rel(", param_x, ")")
+    has_slope = any(c(param_x, rel_param_x) %in% RHS)
+    formula_str = paste0(formula_str, "\n# Segment ", i, ": ~ ", paste0(RHS, collapse = " + "), "\n")
+
+    # Prepare change point
+    if(i > 1) {
+      cp_name = paste0("cp_", i-1)
+      min_val = ifelse(i > 2, paste0("cp_", i-2), "MINX")
+      default_prior[[cp_name]] = paste0("dunif(", min_val, ", MAXX)")
+
+      # If there is a user-supplied prior that is not dunif nor truncated, add truncation
+      if(cp_name %in% names(prior)) {
+        if(!stringr::str_detect(prior[[cp_name]], "dunif|T\\(")) {
+          prior[[cp_name]] = paste0(prior[[cp_name]], " T(cp_", i-2, ", )")
+        }
+      }
+    }
+
+    # Prepare indicators. ind_abs = absolute, ind_rel = relative
+    # PARAM_X will be replaced by the real name. FUTURE_REL will sometimes be
+    # replaced by a less-than indicator (ind_past).
+
+    ind_this = paste0("(PARAM_X >= cp_", i - 1 , ") * FUTURE_REL")
+    ind_past = paste0("(PARAM_X < cp_", i - 1, ") * ")
+
+    # Add intercept
+    if(has_intercept) {
+      int_name = paste0("int_", i)
+      default_prior[[int_name]] = default_prior_int
+
+      # "remove" earlier terms for absolute intemin(rcepts
+      if("1" %in% RHS) {
+        formula_str = gsub("FUTURE_REL", ind_past, formula_str)
+      }
+
+      formula_str = paste0(formula_str, ind_this, int_name, " + \n")
+    }
+
+    # Add slope
+    if(has_slope) {
+      slope_name = paste0(param_x, "_", i)
+      default_prior[[slope_name]] = default_prior_slope
+
+      # If "slope", slope_str = this_slope. If "rel(slope)", slope_str = (this_slope - last_slope)
+      slope_str = slope_name  # candidate until proven wrong
+      if(rel_param_x %in% RHS) {  # If rel(slope) in formula
+        past_slope_name = paste0(param_x, "_", i-1)
+        if(past_slope_name %in% names(default_prior)) {  # if the last segment had a slope
+          slope_str = paste0("(", slope_name, " + ", param_x, "_", i-1, ")")
+        }
+      }
+
+      #formula_str = paste0(formula_str, indicator, "((min(", slope_name, ", cp_", i, ") - cp_", i-1, ") + \n")
+      subract_cp = ifelse(i == 1, "",  paste0("- cp_", i-1))
+      formula_str = paste0(formula_str, ind_this, "(", slope_str, " * (min(PARAM_X, cp_", i, ")", subract_cp, ")) + \n")
+    }
+
+    # Finish up formula_str
+    if(i == length(segments)) {
+      formula_str = substr(formula_str, 1, nchar(formula_str) - 3)
+    }
+    if(!has_intercept & !has_slope) {
+      formula_str = paste0(formula_str, "0 + \n")
+    }
+  }
+
+  # Overwrite default with user's prior. Add truncation to cp_ if not already
+  prior = modifyList(default_prior, prior)
+
+  # Remove all "unrealized" FUTURE_REL
+  formula_str = gsub("FUTURE_REL", "", formula_str)
+
+  # Substitute to get a formula to insert in JAGS
+  formula_jags = gsub("PARAM_X", paste0(param_x, "[i_]"), formula_str)
+
+  # Turn formula_str into a proper R function
+  formula_func = gsub("PARAM_X", param_x, formula_str)  # Proper param_x name
+  formula_func = gsub("min\\(", "pmin\\(", formula_func)  # vectorized mean for function
+
+  func_str = paste0(
+  "function(", param_x, ", ", paste0(names(default_prior), collapse=", "), ", type=\"predict\") {
+    # Helpers to simplify making the code for this function
+    cp_0 = -Inf
+    cp_", length(segments), " = Inf
+
+    # Fitted value
+    y = ", formula_func, "
+
+    # Return predictions or fitted values?
+    if(type == 'predict') return(rnorm(length(", param_x, "), y, sigma))
+    if(type == 'fitted') return(y)
+    if(!type %in% c('predict', 'fitted')) stop(\"`type` must be one of 'predict' or 'fitted'\")
+  }")
+
+  func_y = eval(parse(text = func_str))
+
+
+  return(list(
+    prior = modifyList(default_prior, prior),
+    formula_jags = formula_jags,
+    func_y = func_y,
+    param_x = param_x,
+    param_y = param_y
+  ))
+}
