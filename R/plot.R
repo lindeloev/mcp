671c212645c86bbe4dde68289b717593ddab343b
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2019-10-28 08:46:13 +0100

    Rename "x" to "fit" in plot() to avoid confusion

--- R/plot.R
+++ R/plot.R
@@ -27,20 +27,23 @@
 #' }
 
 plot.mcpfit = function(x, type="overlay", draws=25, pars="population", facet_by = NULL, ...) {
+  # Stick to more conventional mcp terms. De-confuses "fit" and "x-axis".
+  fit = x
+
   # Check arguments
-  if (class(x) != "mcpfit")
-    stop("Can only plot mcpfit objects. x was class: ", class(x))
+  if (class(fit) != "mcpfit")
+    stop("Can only plot mcpfit objects. x was class: ", class(fit))
   if (! type %in% c("overlay", "combo"))
     stop("Type has to be one of 'overlay' or 'combo'. Was: ", type)
   if (draws < 1)
     stop("Draws has to be a positive integer.")
   if (!is.null(pars)) {
-    if (pars != "population" & !all(pars %in% c(x$pars$population, x$pars$varying)))
+    if (pars != "population" & !all(pars %in% c(fit$pars$population, fit$pars$varying)))
       stop("Not all these pars are in the model: '", paste0(pars, collapse="' and '"))
   }
 
   # TEMPORARY: Include test of whether this is a random/nested effect
-  varying_groups = logical0_to_null(unique(stats::na.omit(x$.other$ST$cp_group_col)))
+  varying_groups = logical0_to_null(unique(stats::na.omit(fit$.other$ST$cp_group_col)))
   if (!is.null(facet_by)) {
     if (!facet_by %in% varying_groups)
       stop("facet_by is not a data column used as varying grouping.")
@@ -50,15 +53,16 @@ plot.mcpfit = function(x, type="overlay", draws=25, pars="population", facet_by 
   if (type == "overlay") {
 
     # We'll need these vars during data-processing-before-ggplot
-    func_y = x$func_y
-    eval_at = seq(min(x$data[, x$pars$x]),
-                  max(x$data[, x$pars$x]),
+    func_y = fit$func_y
+    eval_at = seq(min(fit$data[, fit$pars$x]),
+                  max(fit$data[, fit$pars$x]),
                   length.out = 100)
-    pars_population_regex = paste0(x$pars$population, collapse="|")
+    pars_population_regex = paste0(fit$pars$population, collapse="|")
+    cp_vars = paste0("cp_", seq_len(length(fit$segments) - 1))  # change point columns
 
     # No faceting
     if (is.null(facet_by)) {
-      Q = x$samples %>%
+      Q = fit$samples %>%
         tidybayes::spread_draws(!!rlang::sym(pars_population_regex), regex = TRUE) %>%
         # TO DO: use spread_draws(n = draws) when tidybayes 1.2 is out
         tidybayes::sample_draws(draws)
@@ -66,10 +70,10 @@ plot.mcpfit = function(x, type="overlay", draws=25, pars="population", facet_by 
     } else {
       # Prepare for faceting
       # Read more about this weird syntax at https://github.com/mjskay/tidybayes/issues/38
-      varying_by_facet = stats::na.omit(x$.other$ST$cp_group[stringr::str_detect(x$.other$ST$cp_group, paste0("_", facet_by))])
+      varying_by_facet = stats::na.omit(fit$.other$ST$cp_group[stringr::str_detect(fit$.other$ST$cp_group, paste0("_", facet_by))])
       varying_by_facet = paste0(varying_by_facet, collapse="|")
 
-      Q = x$samples %>%
+      Q = fit$samples %>%
         tidybayes::spread_draws(!!rlang::sym(pars_population_regex),
                      (!!rlang::sym(varying_by_facet))[!!rlang::sym(facet_by)],
                      regex = TRUE) %>%
@@ -79,25 +83,25 @@ plot.mcpfit = function(x, type="overlay", draws=25, pars="population", facet_by 
 
     # First, let's get all the predictors in shape for func_y
     Q = Q %>%
-      tidyr::expand_grid(!!x$pars$x := eval_at) %>%  # correct name of x-var
+      tidyr::expand_grid(!!fit$pars$x := eval_at) %>%  # correct name of x-var
 
       # Add fitted draws (vectorized)
-      dplyr::mutate(!!x$pars$y := purrr::invoke(func_y, ., type = "fitted")) %>%
+      dplyr::mutate(!!fit$pars$y := purrr::invoke(func_y, ., type = "fitted")) %>%
 
-      # Add line ID to separate lines. Mark a new line when "eval_at" repeats.
+      # Add line ID to separate lines. Mark a new line when "eval_at" repeats
       dplyr::mutate(
-        line = !!dplyr::sym(x$pars$x) == min(eval_at),
+        line = !!dplyr::sym(fit$pars$x) == min(eval_at),
         line = cumsum(line)
       )
 
     if (is.null(facet_by)) {
       # Return plot without faceting
-      return(ggplot(x$data, aes_string(x = x$pars$x, y = x$pars$y)) +
+      return(ggplot(fit$data, aes_string(x = fit$pars$x, y = fit$pars$y)) +
         geom_point() +
         geom_line(aes(group = line), data = Q, color = grDevices::rgb(0.5, 0.5, 0.5, 0.4)))
     } else {
       # Return plot with faceting
-      return(ggplot(x$data, aes_string(x = x$pars$x, y = x$pars$y)) +
+      return(ggplot(fit$data, aes_string(x = fit$pars$x, y = fit$pars$y)) +
         geom_point() +
         geom_line(aes(group = line), data = Q, color = grDevices::rgb(0.5, 0.5, 0.5, 0.4)) +
         facet_wrap(paste0("~", facet_by)))
@@ -107,13 +111,13 @@ plot.mcpfit = function(x, type="overlay", draws=25, pars="population", facet_by 
   if (type == "combo") {
     # Use population parameters by default
     if (pars == "population") {
-      pars = x$pars$population
+      pars = fit$pars$population
     }
     if (length(pars) > 1) {
       #pars = paste0(paste0("^", pars, "$"), collapse="|")
-      return(bayesplot::mcmc_combo(x$samples, pars = pars))
+      return(bayesplot::mcmc_combo(fit$samples, pars = pars))
     } else {
-      return(bayesplot::mcmc_combo(x$samples, regex_pars = pars))
+      return(bayesplot::mcmc_combo(fit$samples, regex_pars = pars))
     }
   }
 }
