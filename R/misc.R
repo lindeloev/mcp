0b85b05d473c2b756bb34261e46a0ba5e29cabce
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2019-11-26 14:22:23 +0100

    First step towards flexible link functions

--- R/misc.R
+++ R/misc.R
@@ -4,11 +4,72 @@
 #' @param link Link function.
 #' @export
 #'
+
 bernoulli = function(link = "logit") {
-  out = list(
-    family = "bernoulli",
-    link = "logit"
-  )
+  get_family("bernoulli", link)
+}
+
+
+#' A family object to store link functions between R and JAGS.
+#'
+#' This will make more sense once more link functions / families are added.
+#'
+#' @aliases get_family
+#' @param name Name of the family
+#' @param link Link function name. Accepts "logit", "log", and "identity"
+get_family = function(name, link) {
+  # Exciting!
+  if (link == "logit") {
+    out = list(
+      family = name,
+      link = "logit",
+      link_jags = "logit",
+      link_r = "logit",  # included in mcp
+      linkinv_jags = "ilogit",
+      linkinv_r = "ilogit"  # included in mcp
+    )
+  }
+
+  # Pretty boring
+  if (link == "log") {
+    out = list(
+      family = name,
+      link = "log",
+      link_jags = "log",
+      link_r = "log",
+      linkinv_jags = "exp",
+      linkinv_r = "exp"
+    )
+  }
+
+  # Pretty boring
+  if (link == "identity") {
+    out = list(
+      family = name,
+      link = "",
+      link_jags = "",
+      link_r = "",
+      linkinv_jags = "",
+      linkinv_r = ""
+    )
+  }
+
   class(out) = "family"
   return(out)
 }
+
+
+
+
+
+#' Inverse logit function
+#'
+#' @aliases ilogit
+#' @param x A vector of logits
+ilogit = stats::binomial(link = "logit")$linkinv
+
+#' Logit function
+#'
+#' @aliases logit
+#' @param x A vector of probabilities (0-1)
+logit = stats::binomial(link = "logit")$linkfun
