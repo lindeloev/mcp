c7a7e2c71c70dbcc69c4c07046b36495650bcb7a
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2019-11-29 10:43:38 +0100

    Document and format all example data

--- data-raw/ex_trig.R
+++ data-raw/ex_trig.R
@@ -5,12 +5,20 @@ segments = list(
 )
 
 # Simulate the data
-empty = mcp(segments, sample = FALSE)
+empty = mcp::mcp(segments, sample = FALSE)
 set.seed(40)
 ex_trig = tibble::tibble(
   x = seq(0, 35, by = 0.15),
-  y = empty$simulate(x, cos_x_2 = 8, cp_1 = 16.5, int_1 = 10, sigma = 3, sin_x_1 = 10, x_2 = 3)
+  y = empty$simulate(
+    x,
+    x_2_cos = 8,
+    cp_1 = 16.5,
+    int_1 = 10,
+    sigma = 3,
+    x_1_sin = 10,
+    x_2 = 3)
 )
 
 # Save to mcp
+ex_trig = data.frame(ex_trig)
 usethis::use_data(ex_trig, overwrite = TRUE)
--- data-raw/ex_variance.R
+++ data-raw/ex_variance.R
@@ -8,16 +8,23 @@ segments = list(
 )
 
 # Simulate data
-empty = mcp(segments, sample = FALSE)
+empty = mcp::mcp(segments, sample = FALSE)
 
 set.seed(30)
 ex_variance = tibble::tibble(
   x = 1:100,
-  y = empty$simulate(x,
-                   cp_1 = 25, cp_2 = 75,
-                   int_1 = 20, x_3 = 2,
-                   sigma_1 = 7, sigma_2 = 25, sigma_x_2 = -0.45))
+  y = empty$simulate(
+    x,
+    cp_1 = 25,
+    cp_2 = 75,
+    int_1 = 20,
+    x_3 = 2,
+    sigma_1 = 7,
+    sigma_2 = 25,
+    sigma_x_2 = -0.45)
+  )
 
 
 # Save it to package
+ex_variance = data.frame(ex_variance)
 usethis::use_data(ex_variance, overwrite = TRUE)
