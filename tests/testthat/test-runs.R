8c3c4029a2e3c44f59209d6b8ae4dba1d2d1626d
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2019-12-28 04:01:15 +0100

    Fix test error

--- tests/testthat/test-runs.R
+++ tests/testthat/test-runs.R
@@ -191,9 +191,9 @@ test_plot = function(fit, varying_cols) {
   q_predict = rbinom(1, 1, 0.5) == 1  # add quantiles sometimes
   # To facet or not to facet
   if (length(varying_cols) > 0) {
-    gg = try(plot(fit, facet_by = varying_cols[1], q_fit = q_fit, q_predict = q_predict), silent = TRUE)  # just take the first
+    gg = try(plot(fit, facet_by = varying_cols[1], q_fit = q_fit, q_predict = q_predict, lines = 3), silent = TRUE)  # just take the first
   } else {
-    gg = try(plot(fit, q_fit = q_fit, q_predict = q_predict), silent = TRUE)
+    gg = try(plot(fit, q_fit = q_fit, q_predict = q_predict, lines = 3), silent = TRUE)
   }
   # Is it a ggplot or a known error?
   if (inherits(gg, "try-error")) {
