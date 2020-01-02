df1027db41bc491c43effc62135ab73423258f46
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2020-01-02 12:08:17 +0100

    Test more priors and remove obsolete code

--- tests/testthat/test-runs.R
+++ tests/testthat/test-runs.R
@@ -165,7 +165,7 @@ test_runs = function(segments,
 
 # Tests if summary(fit) and ranef(fit) work as expected
 test_summary = function(fit, varying_cols) {
-  summary_cols = c('name','mean','lower','upper','Rhat','n.eff','ts_se')
+  summary_cols = c('name','mean','lower','upper','Rhat','n.eff')
   result = purrr::quietly(summary)(fit)$result  # Do not print to console
   output = purrr::quietly(summary)(fit)$output  # Do not print to console
   testthat::expect_true(all(colnames(result) %in% summary_cols))  # All columns
@@ -284,23 +284,55 @@ test_good = function(segments_list, title, ...) {
 ###############
 # TEST PRIORS #
 ###############
-good_prior_segments = list(
+prior_segments = list(
   y ~ 1 + x,
   1 + (1|id) ~ rel(1) + rel(x),
   rel(1) ~ 0
 )
-good_prior = list(
+
+bad_prior = list(
   list(
+    cp_1 = "dirichlet(1)",  # Has to be all-dirichlet
+    cp_2 = "dnorm(3, 10)"
+  ),
+  list(
+    cp_1 = "dirichlet(1)",
+    cp_2 = "dirichlet(0)"  # alpha has to be > 0
+  )
+)
+
+for (prior in bad_prior) {
+  test_name = paste0("Bad priors: ", paste0(prior, collapse=", "))
+  testthat::test_that(test_name, {
+    testthat::expect_error(test_runs(prior_segments, sample = FALSE, prior = prior))
+  })
+}
+
+
+good_prior = list(
+  list(  # Fixed values and non-default change point
     int_2 = "int_1",
     cp_1 = "dnorm(3, 10)",
     x_2 = "-0.5"
+  ),
+  list(  # Outside the observed range allowed
+    cp_1 = "dunif(-100, -90)",
+    cp_2 = "dnorm(100, 20) T(100, 110)"
+  ),
+  list(
+    cp_1 = "dirichlet(1)",  # Dirichlet prior on change points
+    cp_2 = "dirichlet(1)"
+  ),
+  list(
+    cp_1 = "dirichlet(3)",  # Dirichlet prior on change points
+    cp_2 = "dirichlet(2)"
   )
 )
 
 for (prior in good_prior) {
   test_name = paste0("Good priors: ", paste0(prior, collapse=", "))
   testthat::test_that(test_name, {
-    test_runs(good_prior_segments, prior = prior)
+    test_runs(prior_segments, prior = prior)
   })
 }
 
