3121aded1dc1b8602a174ecb2ef7ce96c7072294
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2020-01-05 23:25:36 +0100

    Update site

--- docs/reference/figures/make_README_plots.R
+++ docs/reference/figures/make_README_plots.R
@@ -22,12 +22,12 @@ save_it = function(filename) {
 library(mcp)
 options(mc.cores = 3)  # Run in parallel
 
-segments_demo = list(
+model_demo = list(
   response ~ 1,  # plateau (int_1)
   ~ 0 + time,  # joined slope (time_2) at cp_1
   ~ 1 + time  # disjoined slope (int_1, time_2) at cp_2
 )
-fit_demo = mcp(segments_demo, data = ex_demo)  # dataset included in mcp
+fit_demo = mcp(model_demo, data = ex_demo)  # dataset included in mcp
 theme_it(plot(fit_demo), "")
 save_it("ex_demo.png")
 
@@ -36,11 +36,11 @@ save_it("ex_demo_combo.png")
 
 # LOO
 # Fit the model
-segments_null = list(
+model_null = list(
   response ~ 1 + time,
   ~ 1 + time
 )
-fit_null = mcp(segments_null, ex_demo)
+fit_null = mcp(model_null, ex_demo)
 
 # Compare loos:
 fit_demo$loo = loo(fit_demo)
@@ -51,11 +51,11 @@ loo::loo_compare(fit_demo$loo, fit_null$loo)
 ################
 # Two plateaus #
 ################
-segments_plateaus = list(
+model_plateaus = list(
   y ~ 1,  # plateau (int_1)
   ~ 1  # plateau (int_2)
 )
-fit_plateaus = mcp(segments_plateaus, ex_plateaus, par_x = "x")
+fit_plateaus = mcp(model_plateaus, ex_plateaus, par_x = "x")
 theme_it(plot(fit_plateaus, lines = 25), "Two plateaus")
 save_it("ex_plateaus.png")
 
@@ -65,11 +65,11 @@ save_it("ex_plateaus.png")
 # VARYING SLOPE CHANGE #
 ########################
 
-segments_varying = list(
+model_varying = list(
   y ~ 1 + x,  # intercept + slope
   1 + (1|id) ~ 0 + x + sigma(1)  # joined slope, varying by id
 )
-fit_varying = mcp(segments_varying, ex_varying)
+fit_varying = mcp(model_varying, ex_varying)
 theme_it(plot(fit_varying, facet_by = "id", cp_dens = FALSE), "Varying slope change")
 save_it("ex_varying.png")
 
@@ -77,12 +77,12 @@ save_it("ex_varying.png")
 ############
 # BINOMIAL #
 ############
-segments_binomial = list(
+model_binomial = list(
   y | trials(N) ~ 1,  # constant rate
   ~ 0 + x,  # joined changing rate
   ~ 1 + x  # disjoined changing rate
 )
-fit_binomial = mcp(segments_binomial, ex_binomial, family = binomial(), adapt = 5000)
+fit_binomial = mcp(model_binomial, ex_binomial, family = binomial(), adapt = 5000)
 theme_it(plot(fit_binomial, q_fit = TRUE), "Binomial")
 save_it("ex_binomial.png")
 
@@ -90,7 +90,7 @@ save_it("ex_binomial.png")
 ##########################
 # FIXED, RELATIVE, PRIOR #
 ##########################
-segments_rel = list(
+model_rel = list(
   y ~ 1 + x,
   ~ rel(1) + rel(x),
   rel(1) ~ 0 + x
@@ -102,7 +102,7 @@ prior_rel = list(
   cp_1 = "dunif(20, 50)"  # has to occur in this interval
 )
 
-fit_rel = mcp(segments_rel, ex_rel_prior, prior_rel, iter = 10000)
+fit_rel = mcp(model_rel, ex_rel_prior, prior_rel, iter = 10000)
 theme_it(plot(fit_rel, cp_dens = FALSE), "rel() and prior")
 save_it("ex_fix_rel.png")
 
@@ -110,11 +110,11 @@ save_it("ex_fix_rel.png")
 #############
 # QUADRATIC #
 #############
-segments_quadratic = list(
+model_quadratic = list(
   y ~ 1,
   1 ~ 0 + x + I(x^2)
 )
-fit_quadratic = mcp(segments_quadratic, ex_quadratic)
+fit_quadratic = mcp(model_quadratic, ex_quadratic)
 theme_it(plot(fit_quadratic), "Quadratic and other exponentiations")
 save_it("ex_quadratic.png")
 
@@ -124,11 +124,11 @@ save_it("ex_quadratic.png")
 # TRIGONOMETRIC #
 #################
 # Define the model
-segments_trig = list(
+model_trig = list(
   y ~ 1 + sin(x),
   ~ 0 + cos(x) + x
 )
-fit_trig = mcp(segments_trig, ex_trig)
+fit_trig = mcp(model_trig, ex_trig)
 theme_it(plot(fit_trig), "Trigonometric for periodic trends")
 save_it("ex_trig.png")
 
@@ -137,13 +137,13 @@ save_it("ex_trig.png")
 ############
 # VARIANCE #
 ############
-segments_variance = list(
+model_variance = list(
   y ~ 1,
   ~ 0 + sigma(1 + x),
   ~ 0 + x
 )
 
-fit_variance = mcp(segments_variance, ex_variance, iter = 10000, adapt = 10000)
+fit_variance = mcp(model_variance, ex_variance, iter = 10000, adapt = 10000)
 theme_it(plot(fit_variance, q_predict = TRUE), "Variance and prediction intervals")
 save_it("ex_variance.png")
 
@@ -152,12 +152,12 @@ save_it("ex_variance.png")
 #########
 # AR(N) #
 #########
-segments_ar = list(
+model_ar = list(
   price ~ 1 + ar(2),
   ~ 0 + time + ar(1)
 )
 
-fit_ar = mcp(segments_ar, ex_ar)
+fit_ar = mcp(model_ar, ex_ar)
 plot_ar = plot(fit_ar)
 theme_it(plot_ar, "Time series with autoregressive residuals")
 save_it("ex_ar.png")
