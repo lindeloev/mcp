5d3788bfa24b508ad384ba008cf2d02e707aa7b5
Jonas Lindeløv <jonas@lindeloev.dk>
2021-11-18 10:55:03 +0100

    New tests: validate binomial against glm()

---
+++ tests/testthat/test-validate-binomial.R
@@ -0,0 +1,38 @@
+#############
+# INFERENCE #
+#############
+model = list(
+  y | trials(N) ~ 1 + x + group
+)
+
+
+
+df = tibble::tibble(
+  N = round(runif(200, 1, 10)),
+  x = seq(0, 20, length.out = 200),
+  group = rep(c("A", "B"), 100),
+  y = rbinom(200, N, ilogit(2 - 0.1 * x + ifelse(group == "B", -1, 0)))
+)
+
+fit_mcp = mcp(model, df, family = binomial(), adapt = 100, iter = 1000)
+
+fit_glm = glm(cbind(y, N - y) ~ x + group, data = df, family = binomial())
+
+testthat::expect_equal(
+  fixef(fit_mcp)$mean,
+  as.numeric(fit_glm$coefficients[c(3, 1, 2)]),
+  tolerance = 0.02
+)
+
+
+
+##############
+# SIMULATION #
+##############
+newdata = dplyr::select(df, -y) %>% tidyr::expand_grid(rep = c(1:100))
+newdata$y = fit_mcp$simulate(fit_mcp, newdata, Intercept_1 = 2, x_1 = -0.1, groupB_1 = -1)
+
+fit_glm_sim = glm(cbind(y, N - y) ~ x + group, data = newdata, family = binomial())
+
+
+testthat::expect_equal(as.numeric(fit_glm_sim$coefficients), c(2, -0.1, -1), tolerance = 0.02)
