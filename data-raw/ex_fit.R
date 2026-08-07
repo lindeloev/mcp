ae619d3b9b35f2367ae74de698aadc547f9e37bc
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2026-08-08 01:54:19 +0200

    DOC: Update demo_fit

--- data-raw/ex_fit.R
+++
@@ -1,5 +0,0 @@
-ex = mcp_example("demo", sample = FALSE)
-demo_fit = mcp(ex$model, data = ex$data, adapt = 3000, iter = 1000, sample = "both")
-
-# Save to mcp
-usethis::use_data(demo_fit, overwrite = TRUE)
---
+++ data-raw/make_demo_fit.R
@@ -0,0 +1,7 @@
+devtools::load_all()
+ex = mcp_example("demo", sample = FALSE)
+set.seed(40)
+demo_fit = mcp(ex$model, data = ex$data, adapt = 3000, iter = 1000, chains = 2, sample = "both")
+
+# Save to mcp
+usethis::use_data(demo_fit, overwrite = TRUE)
