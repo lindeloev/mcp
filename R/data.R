e5a1d704fe0249edcfe6dc717a3de49cf86f0ee3
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2026-08-09 00:57:25 +0200

    DOC: document data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,12 @@
-#' Example `mcpfit`
+#' Pre-fitted example mcp model
 #'
-#' This was generated using `demo_fit = mcp_example("demo")`.
+#' `demo_fit` is an `mcpfit` object generated using `mcp_example("demo", sample = FALSE)`,
+#' though with two modifications: it only has 2 x 1000 samples, and it also includes prior samples.
+#'
+#' It is used to quickly demonstrate `mcp` functionality in various documentation.
 #'
 #' @format An \code{\link{mcpfit}} object.
+#' @seealso \code{\link{mcp_example}} for the model and data used to create it,
+#'   and \code{\link{mcpfit-class}} for the structure and methods of fitted models.
 #'
 "demo_fit"
