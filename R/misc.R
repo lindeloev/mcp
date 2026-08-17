03cdb268ab1e522c5064f8b8b88781cd951b31dc
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2026-08-17 10:44:55 +0200

    chore: improve release questions

--- R/misc.R
+++ R/misc.R
@@ -64,13 +64,14 @@ release_questions = function() {
     "TEST: Have you run the release-level fit recovery tests? Sys.setenv(MCP_TEST_LEVEL = 'release', TESTTHAT_CPUS = 11); devtools::test()",
     "TEST: Have you manually reviewed all mcp_example() plots?",
     "TEST: Have you run `dev/ai-check-jagscode.R` to start LLM review?",
-    "TEST: Have you run `revdepcheck::revdep_check()`?",
+    "TEST: Have you run all dev/prompt-*.R scripts to get expert review?",
+    "TEST: Have you run `revdepcheck::revdep_check()` and notified?",
 
     "DATA: Have you generated a new `demo_fit` if needed?",
 
     "DOC: Have you rendered and checked the README? rmarkdown::render('README.Rmd')",
     "DOC: Have you re-built the site using pkgdown::build_site() AFTER deleting caches of articles in 'vignettes/*_cache/'?",
-    "DOC: Have you checked all articles and plots after re-building the site?",
+    "DOC: Have you pushed to dev-branch and checked all pages in https://lindeloev.github.io/mcp/dev/ render correctly?",
     "DOC: Have you run the script to insert the correct logo.png in the HTML meta?"
   )
 }
