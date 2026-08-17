f6661e81a96b8ed8a3c4f7de34bca093fabed2d4
Jonas Kristoffer Lindeløv <jonas@lindeloev.dk>
2026-08-18 00:13:08 +0200

    BUG: update demo_fit

--- data-raw/make_demo_fit.R
+++ data-raw/make_demo_fit.R
@@ -1,7 +1,7 @@
 devtools::load_all()
 ex = mcp_example("demo", sample = FALSE)
 set.seed(40)
-demo_fit = mcp(ex$model, data = ex$data, adapt = 3000, iter = 1000, chains = 2, sample = "both", seed = 40)
+demo_fit = mcp(ex$model, data = ex$data, warmup = 3000, iter = 1000, chains = 2, sample = "both", seed = 40)
 
 # Save to mcp
 usethis::use_data(demo_fit, overwrite = TRUE)
